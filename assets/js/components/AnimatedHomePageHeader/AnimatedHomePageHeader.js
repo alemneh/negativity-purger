b94f710657827946705d96f7d5ae9047a9e115dc
Alemneh <Alemneh@hotmail.com>
2017-03-29 15:43:26 -0700

    created animated header

---
+++ assets/js/components/AnimatedHomePageHeader/AnimatedHomePageHeader.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './AnimatedHomePageHeader.scss';
+
+const AnimatedHomePageHeader = () => {
+  return (
+    <div className={styles.textContainer}>
+      <span className={styles.regText}>Purge </span>
+      <span className={styles.letter}>N</span>
+      <span className={styles.letter}>e</span>
+      <span className={styles.letter}>g</span>
+      <span className={styles.letter}>a</span>
+      <span className={styles.letter}>t</span>
+      <span className={styles.letter}>i</span>
+      <span className={styles.letter}>v</span>
+      <span className={styles.letter}>i</span>
+      <span className={styles.letter}>t</span>
+      <span className={styles.letter}>y</span>
+    </div>
+  )
+};
+
+
+export default AnimatedHomePageHeader;
