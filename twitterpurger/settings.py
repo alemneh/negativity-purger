1ce4c1ea5e6a921e0abdabb90d5f47ef159b89bc
Alemneh <Alemneh@hotmail.com>
2017-03-30 23:56:23 -0700

    small change to settings.py

--- twitterpurger/settings.py
+++ twitterpurger/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
-# Quick-start development settings - unsuitable for production
+# Quickstart development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
