2c2186edbfa6a381f13e153a6fddf3465ea213d5
Alemneh <Alemneh@hotmail.com>
2017-03-30 23:41:22 -0700

    ready for deployment!!

--- twitterpurger/settings.py
+++ twitterpurger/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 
@@ -82,7 +83,12 @@ WSGI_APPLICATION = 'twitterpurger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 AUTHENTICATION_BACKENDS = (
 
