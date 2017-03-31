d1b72edb6ea8f6fa38e19e694eaeaa84390ec88a
Alemneh <Alemneh@hotmail.com>
2017-03-31 00:44:35 -0700

    fixed static root

--- twitterpurger/settings.py
+++ twitterpurger/settings.py
@@ -109,7 +109,7 @@ STATICFILES_DIRS = (
 
 WEBPACK_LOADER = {
     'DEFAULT': {
-        'BUNDLE_DIR_NAME': 'assets/bundles/',
+        'BUNDLE_DIR_NAME': 'bundles/',
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
     }
 }
