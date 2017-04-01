1260564cec588b8674d291eedf533930da762a1f
Alemneh <Alemneh@hotmail.com>
2017-04-01 00:32:16 -0700

    changed statict root to assests

--- twitterpurger/settings.py
+++ twitterpurger/settings.py
@@ -102,6 +102,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = (
     #This lets Django's collectstatic store our bundles
@@ -132,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 LOGIN_REDIRECT_URL = "/purge/"
 
--- twitterpurger/urls.py
+++ twitterpurger/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
