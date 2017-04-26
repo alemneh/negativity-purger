77ead72920727290a28c30dccb9a889790370ccb
Alemneh <Alemneh@hotmail.com>
2017-04-25 23:49:18 -0700

    added test unit

---
+++ tests/unittst.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestBasic(unittest.TestCase):
+    "Basic tests"
+
+    def test_basic(self):
+        a = 1
+        self.assertEqual(1, a)
+
+    def test_basic_2(self):
+        a = 1
+        asert a == 1
